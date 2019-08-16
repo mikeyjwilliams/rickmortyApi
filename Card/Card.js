f679e631c8fe79c12824e98a045543ed5f82d388
Michael Williams <manicdesigner@gmail.com>
2019-08-16 14:44:28 -0400

    created rest of card in CardCreator function and returned it tested in axios

--- Card/Card.js
+++ Card/Card.js
@@ -10,12 +10,35 @@ function CardCreator(obj) {
   const characterName = document.createElement('h2');
   characterName.textContent = obj.name;
   card.appendChild(characterName);
+
+  const origin = document.createElement('p');
+  origin.textContent = obj.origin.name;
+  card.appendChild(origin);
+
+  const location = document.createElement('p');
+  location.textContent = obj.location.name;
+  card.appendChild(location);
+
+  const status = document.createElement('p');
+  status.textContent = obj.status;
+  card.appendChild(status);
+
+  const species = document.createElement('p');
+  species.textContent = obj.species;
+  card.appendChild(species);
+
+  return card;
 }
 
 axios
   .get('https://rickandmortyapi.com/api/character/2')
   .then(response => {
     console.log(response.data);
+
+    const morty = response.data;
+
+    const mortyCard = CardCreator(morty);
+    console.log(mortyCard);
   })
   .catch(err => {
     console.log(err);
