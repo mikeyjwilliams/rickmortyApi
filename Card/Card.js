78f1df176a03a36f010b50201a17a13fd84b4dd8
Michael Williams <manicdesigner@gmail.com>
2019-08-17 19:43:06 -0400

    map over data from page = 1 return card creator data forEach cards append to mainSection class

--- Card/Card.js
+++ Card/Card.js
@@ -1,48 +1,54 @@
 function CardCreator(obj) {
-  const card = document.createElement('div');
-  card.classList.add('card');
+	const card = document.createElement('div');
+	card.classList.add('card');
 
-  const image = document.createElement('img');
-  image.classList.add('card-img');
-  image.src = obj.image;
-  image.alt = obj.name;
-  card.appendChild(image);
+	const image = document.createElement('img');
+	image.classList.add('card-img');
+	image.src = obj.image;
+	image.alt = obj.name;
+	card.appendChild(image);
 
-  const characterName = document.createElement('h2');
-  characterName.textContent = obj.name;
-  card.appendChild(characterName);
+	const characterName = document.createElement('h2');
+	characterName.textContent = obj.name;
+	card.appendChild(characterName);
 
-  const origin = document.createElement('p');
-  origin.textContent = obj.origin.name;
-  card.appendChild(origin);
+	const origin = document.createElement('p');
+	origin.textContent = obj.origin.name;
+	card.appendChild(origin);
 
-  const location = document.createElement('p');
-  location.textContent = obj.location.name;
-  card.appendChild(location);
+	const location = document.createElement('p');
+	location.textContent = obj.location.name;
+	card.appendChild(location);
 
-  const status = document.createElement('p');
-  status.textContent = obj.status;
-  card.appendChild(status);
+	const status = document.createElement('p');
+	status.textContent = obj.status;
+	card.appendChild(status);
 
-  const species = document.createElement('p');
-  species.textContent = obj.species;
-  card.appendChild(species);
+	const species = document.createElement('p');
+	species.textContent = obj.species;
+	card.appendChild(species);
 
-  return card;
+	return card;
 }
 
 axios
-  .get('https://rickandmortyapi.com/api/character/2')
-  .then(response => {
-    console.log(response.data);
+	.get(`https://rickandmortyapi.com/api/character/?page=1`)
+	.then((response) => {
+		console.log(response.data.results);
+		const data = response.data.results;
+		const cards = data.map((card) => {
+			return CardCreator(card);
+		});
+		console.log(cards);
+		// const morty = response.data;
 
-    const morty = response.data;
-
-    const mortyCard = CardCreator(morty);
-    console.log(mortyCard);
-    const mainSection = document.querySelector('.main-section');
-    mainSection.appendChild(mortyCard);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+		// const mortyCard = CardCreator(morty);
+		// console.log(mortyCard);
+		const mainSection = document.querySelector('.main-section');
+		cards.forEach((card) => {
+			mainSection.appendChild(card);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
